0d2bf6649ffbf028a2adfe611806a8fac678435e
Gabriel Garcia Barboza <153201408+GabrielBarboz4@users.noreply.github.com>
2024-02-01 18:05:04 -0300

    arquivos do projeto

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function alterarStatus(id) {
+    let botaoClicado = document.getElementById (`game-${id}`);
+    let imagem = botaoClicado.querySelector ('.dashboard__item__img');
+    let botao = botaoClicado.querySelector ('.dashboard__item__button');
+    let nomeJogo = botaoClicado.querySelector ('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+    
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+
+    }
+}
